82bc7aad65fef9fd5c8d629ab306c1091c00644a
Aaron Montoya <aatoya7@gmail.com>
2017-10-03 12:41:41 -0600

    Added Javadoc Documentation to the GUI

--- src/view/Display.java
+++ src/view/Display.java
@@ -2,13 +2,27 @@ package view;
 
 import javax.swing.JOptionPane;
 
+/**
+ * Simple GUI class using JOptionPane.
+ * @author Aaron Montoya
+ * @version 1.0 10.03.2017
+ */
 public class Display
 {
+	/**
+	 * Displays a popup showing the supplied String to the user
+	 * @param textToDisplay The Text to be displayed.
+	 */
 	public void displayText(String textToDisplay)
 	{
 		JOptionPane.showMessageDialog(null, textToDisplay);
 	}
 	
+	/**
+	 * Uses a popup to display a question to the user and turns their response as a String.
+	 * @param fromQuestion The question being asked.
+	 * @return The users response to the question
+	 */
 	public String getResponse(String fromQuestion)
 	{
 		String answer = "";
